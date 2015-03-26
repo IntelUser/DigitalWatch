a6c01f93ecda986c10901613a46d8fdc477fcf68
IntelUser <viperbait@gmail.com>
2015-03-26 11:05:28 +0100

    stuff

---
+++ DigitalWatch/DigitalWatch/Watch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalWatch
+{
+    class Watch
+    {
+        private readonly EmailConnector _emailConnector;
+
+        public Watch()
+        {
+            
+        }
+
+        public static void ShowEmailNotification(string message)
+        {
+            
+        }
+    }
+}
